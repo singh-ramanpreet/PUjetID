8406772881e6471c5001648f0d01526fb5252710
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2019-08-16 14:37:26 -0500

    optimize crab submission

--- pileupJetId/test/submit_crab_Data_94X.py
+++ pileupJetId/test/submit_crab_Data_94X.py
@@ -14,7 +14,7 @@ config.Data.inputDataset = ''
 
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 20
+config.Data.unitsPerJob = 100
 config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'
 config.Data.outLFNDirBase = '/store/user/%s/jets_2017/' % (getUsernameFromSiteDB())
 config.Data.publication = False
